7b0559f0e45ca26eec8c1fba429135096c625963
Guilherme0Medeiros <guilherme272004@gmail.com>
2025-06-17 18:02:00 -0300

    alterando tela principal com os pokemons e tela de detalhes

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -1,20 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonService } from '../../services/pokemon.service';
+import { CommonModule, TitleCasePipe } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FavoritosService } from 'src/app/services/favoritos.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, TitleCasePipe],
 })
 export class DetailsPage implements OnInit {
+  pokemon: any = null;
+  isLoading = true;
+  types: string = '';
+  abilities: string = '';
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private pokemonService: PokemonService,
+    private favoritosService: FavoritosService
+  ) {}
 
   ngOnInit() {
+    const name = this.route.snapshot.paramMap.get('name');
+
+    if (name) {
+      this.pokemonService.getPokemonDetails(name).subscribe((data) => {
+        this.pokemon = data;
+
+        // extrai Tipos e habilidades como strings simpless
+        this.types = data.types.map((t: any) => t.type.name).join(', ');
+        this.abilities = data.abilities
+          .map((a: any) => a.ability.name)
+          .join(', ');
+
+        this.isLoading = false;
+      });
+    }
   }
 
+  // retorna se o pokemon atual esta favoritado ou nao
+  isFavorito(): boolean {
+    return this.favoritosService.isFavorito(this.pokemon?.name);
+  }
+
+  // adicionar e remover dos favoritos
+  toggleFavorito(): void {
+    this.favoritosService.toggleFavorito(this.pokemon?.name);
+  }
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../../services/pokemon.service';
+import { Router } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular'; 
+import { NgFor, TitleCasePipe } from '@angular/common';
+import { RouterModule } from '@angular/router'; 
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [
+    IonicModule, 
+    NgFor,
+    TitleCasePipe,
+    RouterModule 
+  ],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  pokemons: any[] = [];
+
+  constructor(private pokeService: PokemonService, private router: Router) {}
+
+  ngOnInit() {
+    this.pokeService.getPokemons(50, 0).subscribe((data) => {
+      this.pokemons = data;
+    });
+  }
+
+  openDetails(name: string) {
+    this.router.navigate(['/details', name]);
+  }
 }
